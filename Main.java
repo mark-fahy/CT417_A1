ee4ef03b61e4414fe646695f75ad62171a7f9bcd
mark-fahy <72506991+mark-fahy@users.noreply.github.com>
2021-11-05 19:13:12 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package CT417_Assignment1_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Mark Fahy
+ */
+public class Main {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<Student> listStudent = new ArrayList<>();
+        listStudent.add(new Student("Jack",20, "BCT", "22-12-2000", 183111, "programming, maths, CT theory")
+        listStudent.add(new Student("Joe",21, "EG", "22-12-1999", 183112, "programming, science, french")
+        listStudent.add(new Student("Conor",20, "BComm", "22-12-2000", 183113, "science,german, maths")
+        listStudent.add(new Student("Ronan",22, "BCT", "22-12-1998", 183114, "programming, french, CT theory")
+                
+                
+        public String toString(){
+            return "Name: " + name + "Age: " + age + "Course: " + course + "Date of Birth: " + dateOfBirth + "Id: " + id + "Username: " + userName + "modules: " + modules;
+        }
+    }
+    
+   
+    
+}
